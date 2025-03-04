4b43c4731c7134f73c84dfac6c86c3b8d155fb9c
Damaris Ngari <wanjirungari2@gmail.com>
2025-03-04 20:09:30 +0300

    models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker, backref
+from sqlalchemy.orm import relationship, sessionmaker
 
 Base = declarative_base()
 
@@ -11,16 +11,19 @@ class Company(Base):
     name = Column(String)
     founding_year = Column(Integer)
 
-    freebies = relationship('Freebie', backref='company')
+    freebies = relationship('Freebie', back_populates='company')
 
-    def give_freebie(self, dev, item_name, value):
-        freebie = Freebie(item_name=item_name, value=value, dev=dev, company=self)
-        session.add(freebie)
-        session.commit()
+    def devs(self):
+        return [freebie.dev for freebie in self.freebies]
 
     @classmethod
-    def oldest_company(cls):
+    def oldest_company(cls, session):
         return session.query(cls).order_by(cls.founding_year).first()
+
+    def give_freebie(self, dev, item_name, value):
+        new_freebie = Freebie(item_name=item_name, value=value, dev=dev, company=self)
+        session.add(new_freebie)
+        session.commit()
 
 class Dev(Base):
     __tablename__ = 'devs'
@@ -28,14 +31,17 @@ class Dev(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
-    freebies = relationship('Freebie', backref='dev')
+    freebies = relationship('Freebie', back_populates='dev')
+
+    def companies(self):
+        return [freebie.company for freebie in self.freebies]
 
     def received_one(self, item_name):
-        return any(freebie.item_name == item_name for freebie in self.freebies)
+        return any([freebie.item_name == item_name for freebie in self.freebies])
 
     def give_away(self, dev, freebie):
         if freebie in self.freebies:
-            freebie.dev = dev 
+            freebie.dev = dev
             session.commit()
 
 class Freebie(Base):
@@ -46,6 +52,9 @@ class Freebie(Base):
     value = Column(Integer)
     dev_id = Column(Integer, ForeignKey('devs.id'))
     company_id = Column(Integer, ForeignKey('companies.id'))
+
+    dev = relationship('Dev', back_populates='freebies')
+    company = relationship('Company', back_populates='freebies')
 
     def print_details(self):
         return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
